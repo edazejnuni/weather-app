46f919cb5befbb16ebeb0558527b65fa91c9a9b8
edazejnuni <edona.zejnuni@gmail.com>
2022-08-31 11:14:19 +0200

    fix

--- src/api.js
+++ src/api.js
@@ -10,3 +10,13 @@ export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
 
 export const WEATHER_API_KEY = "key";
+
+export const climateNewsOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "key",
+    "X-RapidAPI-Host": "climate-news6.p.rapidapi.com",
+  },
+};
+
+export const CLIMATE_NEWS_URL = "https://climate-news6.p.rapidapi.com/news/";
